2b3bb6cfc70a9cb0e5dda0a511f74848d9a17cfb
Emilien Lefevre <emilienlefevre93@gmail.com>
2018-06-18 17:56:41 +0200

    updated the schema and did the seeds or the organisers and the bands

--- db/migrate/20180618132828_create_ratings.rb
+++ db/migrate/20180618132828_create_ratings.rb
@@ -1,8 +1,8 @@
 class CreateRatings < ActiveRecord::Migration[5.2]
   def change
     create_table :ratings do |t|
-      t.references :bands, foreign_key: true
-      t.references :events, foreign_key: true
+      t.references :band, foreign_key: true
+      t.references :event, foreign_key: true
       t.string :content
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "creating organisers"
+10.times do
+    pwd = "teeest"
+  user = User.new(
+    email:    Faker::Internet.email,
+    password: pwd,
+    
+    is_organiser: true,
+    is_band: false,
+    )
+  user.save!
+
+  organiser = Organiser.new(
+    user_id: user.id,
+    name: Faker::HarryPotter.character,
+    )
+  organiser.save!
+end
+
+puts "organisers created"
+
+
+puts "creating bands"
+10.times do 
+  user = User.new(
+    email:    Faker::Internet.email,
+    password: "teeest",
+    is_band: true,
+    is_organiser: false,
+  )
+  user.save
+
+  band = Band.new(
+    user_id: user.id,
+    name: Faker::RockBand.name,
+    content: Faker::StarWars.quote,
+    pictures: "http://igloomusic.co.uk/wp-content/uploads/2014/09/my-band.jpg",
+    soundcloud: "https://soundcloud.com/deadmau5",
+    location: Faker::Address.full_address,
+    )
+end
+
+puts "bands created"
