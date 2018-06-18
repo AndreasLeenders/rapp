d0462604161cfeb316d1709250e41a482a5ea0c1
Andreas Leenders <andreas_leenders@hotmail.com>
2018-06-18 17:57:35 +0200

    first commit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  resources :events, only: [:index, :show]
+  resources :bands
+
+  resources :organisers do
+  resources :events, only: [:create, :edit, :update, :destroy]
+  end
+  resources :bands do
+  resources :bookings
+  end
+  resources :organiser do
+  resources :bookings, only: [:index, :show]
+  end
   get 'events/index'
   get 'events/show'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  end
+
