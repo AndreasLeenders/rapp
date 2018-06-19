7069dc4752935871991f90defdbbd2262e078265
Emilien Lefevre <emilienlefevre93@gmail.com>
2018-06-19 11:56:21 +0200

    created the organiser controller (we still have to test it) and the devise view in order to assigna role to a new user (band or organiser)

--- app/controllers/organisers_controller.rb
+++ app/controllers/organisers_controller.rb
@@ -1,2 +1,49 @@
 class OrganisersController < ApplicationController
+
+  before_action :set_user
+
+  def new
+    @organiser = Organiser.new
+  end
+
+  def create
+    @organiser = organiser.new(organiser_params)
+    @organiser.restaurant = @restaurant
+    if @organiser.save
+      redirect_to organiser_path(@organiser),  notice: 'Organiser was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @organiser.update(organiser_params)
+      redirect_to organiser_path(@organiser), notice: 'Organiser was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = user.id
+  end
+
+  def index
+    @organiser = Organiser.all
+  end
+
+  def destroy
+    @organiser.destroy
+  end
+
+  private
+
+  def organiser_params
+    params.require(:organiser).permit(:name)
+  end
+
+  def set_user
+    @user = current_user.id
+    raise
+  end
 end
