85f3cc96a3e426a7e72869377774ea1b6ed6a3eb
vaibhav kukreja <kukreja.vaibhav@gmail.com>
2018-06-19 12:59:13 +0200

    band-controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,52 +1,59 @@
 class BandsController < ApplicationController
    before_action :set_band, only: [:index, :show, :create, :destroy]
  
- def index 
-  @bands = Band.all
- end
+  def index 
+   @bands = Band.all
+  end
 
- def show
- end
+  def show
+   @band = Band.find(params[:id].to_i)
+  end
 
- def new
-  @band = Band.new
- end
+  def new
+   @band = Band.new
+  end
 
- def edit
- end
+  def edit
+   if !is_band
+    puts "you are not allowed to access"
+    redirect_to bands_path
+  end
+  end
  
- def create
-  @band = Band.new(band_params)
-  if @band.save
+  def create
+   @band = Band.new(band_params)
+   if @band.save
     redirect_to band_path(@band)
-  else
+   else
     render :new
+   end
   end
- end
 
- def update
-  if @band.update(band_params)
-     redirect_to band_path(@band)
-  else
-     render :edit
+  def update
+   if is_band?
+    if @band.update(band_params)
+     redirect_to band_path(@band), notice: 'Band was successfully updated.'
+    else
+       render :edit
+    end
+   end
+
   end
- end
 
- def destroy
-  @band = Band.find(params[:id])
-  @band.destroy
-  redirect_to band_path(@band)
- end
+  def destroy
+   @band = Band.find(params[:id])
+   @band.destroy
+   redirect_to band_path(@band)
+  end
 
 
  private
 
- def set_band
-    @band = Band.find(params[:id])
- end
+  def set_band
+    @band = Band.find(params[:id].to_i)
+  end
 
- def band_params
+  def band_params
     params.require(:band).permit(:name)
- end
-
+  end
 end
