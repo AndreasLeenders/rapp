5dcbe6117358816af55402dd10ff7875a5181585
Olivier Nottebart <olivier.nottebart@gmail.com>
2018-06-21 15:35:29 +0200

    Bug fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,7 +16,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-      @marker = [
+      @markers = [
         {
           lat: @event.latitude,
           lng: @event.longitude#,
