dd6b88c741e4c734d8b53dec4b79d3bc5629c3c1
Olivier Nottebart <olivier.nottebart@gmail.com>
2018-06-21 16:02:47 +0200

    UI enhancements

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,11 +2,10 @@ import GMaps from 'gmaps/gmaps.js';
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0, disableDefaultUI: true });
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   const styles = [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}];
-
 map.addStyle({
   styles: styles,
   mapTypeId: 'map_style'
@@ -22,4 +21,3 @@ map.setStyle('map_style');
     map.fitLatLngBounds(markers);
   }
 }
-
