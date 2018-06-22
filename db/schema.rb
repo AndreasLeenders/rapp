14c1c8a2f6a822705ffcd9d1b60f1633feac6847
Andreas Leenders <andreas_leenders@hotmail.com>
2018-06-22 12:06:47 +0200

    messaging booking in order should maybe check how to prevent band booking event 2 times

--- db/schema.rb
+++ db/schema.rb
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(version: 2018_06_21_151912) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "band_id"
     t.bigint "event_id"
-    t.string "status", default: "pending"
+    t.string "status", default: "Pending"
     t.text "message"
+    t.text "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["band_id"], name: "index_bookings_on_band_id"
