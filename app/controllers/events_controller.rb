754de8d97093d344bfe291e5ed34155eb1ed97d6
AndreasLeenders <38980044+AndreasLeenders@users.noreply.github.com>
2018-06-22 14:56:56 +0200

    Update events_controller.rb
    event.all not correct made it the right one

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :search, :show]
 
   def index
-    @events = Event.all
+    @events = Event..where.not(latitude: nil, longitude: nil)
 
     @markers = @events.map do |event|
       {
